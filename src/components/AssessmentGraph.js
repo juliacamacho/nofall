0b435ddfe1d65c846e1588e849ca723f193e2f4b
Julia Camacho <49563129+juliacamacho@users.noreply.github.com>
2021-02-12 23:36:11 -0600

    add assessments section of dashboard

---
+++ src/components/AssessmentGraph.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+const AssessmentGraph = (props) => {
+    return (
+        <div className="bg-gray-100 rounded-lg py-8 px-10 mb-6">
+
+            <div className="flex justify-between items-center">
+
+                <div className="flex space-x-4 items-baseline">
+                    <h1 className="text-xl font-semibold">{props.title}</h1>
+                    <Link className="text-gray-600 hover:text-indigo-500 text-sm">Edit Goal</Link>
+                </div>
+
+                <div className="flex items-center space-x-6">
+                    <button className="text-lg font-semibold text-indigo-500 hover:text-indigo-400">Year</button>
+                    <button className="text-lg font-semibold text-indigo-500 hover:text-indigo-400">Month</button>
+                    <button className="bg-indigo-500 rounded-lg py-1 px-4 text-lg font-semibold text-white">Week</button>
+                </div>
+
+            </div>
+            
+            {/* graph here */}
+
+        </div>
+    )
+}
+
+export default AssessmentGraph;
--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -6,6 +6,7 @@ import Tabs from "../components/Tabs"
 import ActivityGraph from '../components/ActivityGraph'
 import ActivitySummary from '../components/ActivitySummary'
 import ActivityCard from '../components/ActivityCard'
+import AssessmentGraph from '../components/AssessmentGraph'
 
 const DashboardPage = () => {
     return (
@@ -19,12 +20,15 @@ const DashboardPage = () => {
                 <ActivitySummary />
                 <ActivityGraph title="Time Spent Sitting/Lying Down"/>
                 <ActivityGraph title="Frequency of Stand-ups"/>
-
                 <div className="grid grid-cols-3 gap-x-4">
                     <ActivityCard title="Average Walking Speed" yesterday={3} lastWeek={-4} lastMonth={-14} />
                     <ActivityCard title="Average Sitting/Standing Speed" yesterday={3} lastWeek={-4} lastMonth={-14} />
                     <ActivityCard title="Average Time Spent in Room" yesterday={3} lastWeek={-4} lastMonth={-14} />
                 </div>
+
+                <h1 className="text-2xl font-bold mt-14 mb-6">Proactive Risk Assessment Results</h1>
+                <AssessmentGraph title="Timed Up-and-Go Test"/>
+                <AssessmentGraph title="Chair Stand Test"/>
 
             </div>
         </>
