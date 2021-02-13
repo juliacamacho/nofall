dda06706cf057136000daf23c098a64181cc1513
Eric Zhang <36653830+ez314@users.noreply.github.com>
2021-02-13 16:07:29 -0600

    fix tickvals for line ActivityGraph.js

--- src/components/ActivityGraph.js
+++ src/components/ActivityGraph.js
@@ -4,6 +4,11 @@ import Plot from 'react-plotly.js';
 import {db} from "../firebase";
 // import d3 from "d3-time-format"
 
+const timeVals = [
+    "0:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00",
+    "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00",
+]
+
 function formatTime(t) {
     return `${t}:00`
 }
@@ -132,7 +137,7 @@ const ActivityGraph = (props) => {
                     autosize: true,
                     xaxis: {
                         title: 'Time of Day',
-                        tickvals: standupsX,
+                        tickvals: timeVals,
                         titlefont: {
                             family: 'Inter, sans-serif',
                             size: 18,
