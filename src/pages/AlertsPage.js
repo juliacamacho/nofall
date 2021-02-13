52d032ddd7dd81aa815d8bd21511ff8ceb24be62
Eric Zhang <36653830+ez314@users.noreply.github.com>
2021-02-12 23:57:00 -0600

    alert page + cards

--- src/pages/AlertsPage.js
+++ src/pages/AlertsPage.js
@@ -1,11 +1,46 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Tabs from "../components/Tabs";
+import AlertCard from "../components/AlertCard";
 
 const AlertsPage = () => {
     return (
-        <div>
-            <h1>hello world</h1>
-        </div>
+        <>
+            <Navbar/>
+            <div className="py-8 px-16">
+                <Tabs/>
+                <div className="space-y-3">
+                    <AlertCard
+                        type='info'
+                        message="James has met the sitting/lying down time goal for the day!"
+                        timestamp={1613195383000}
+                    />
+                    <AlertCard
+                        type='alert'
+                        message="James appears to have fallen down!"
+                        timestamp={1613195383000}
+                    />
+                    <AlertCard
+                        type='info'
+                        message="James has finished exercising!"
+                        timestamp={1613105953000}
+                    />
+                    <AlertCard
+                        type='info'
+                        message="James has started exercising!"
+                        timestamp={1613105643000}
+                    />
+                    <AlertCard
+                        type='info'
+                        message="James has exceeded the sitting/lying down time goal for the day!"
+                        timestamp={1613195383000}
+                    />
+                </div>
+
+
+            </div>
+        </>
     )
-}
+};
 
 export default AlertsPage;
