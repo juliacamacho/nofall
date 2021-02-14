627b8dff4ab9fe6b63a161e02058c640385e9e96
Eric Zhang <36653830+ez314@users.noreply.github.com>
2021-02-13 23:54:33 -0600

    new generation script

--- backend/plotting.py
+++ backend/plotting.py
@@ -1,41 +1,45 @@
 from api import logs_ref
-import random
+import random, sys
+
+activityMultipliers = [0.1,0,0,0,0.1,0.1,1,4,7,2,3,4,0,4,3,6,6,2,5,9,2,1,1,0.5]
 
 
-# minutely = []
-# cul = 0
-# for i in range(1440):
-#     if i < 360:
-#         cul += 60
-#     elif i <700:
-#         if random.randint(0,3) <= 2:
-#             cul += random.randint(20,60)
-#         else:
-#             cul += 0
-#     elif i < 842:
-#         cul += 60
-#     elif i < 1000:
-#         cul += 60
-#     elif i < 1154:
-#         cul += random.randint(0,20)
-#     else:
-#         cul += 60
-#     minutely.append(cul)
+minutely = []
+stood = [0]*24
+cul = 0
 
+i = 0
+remaining = 0
 
+for i in range(1440):
+    minutely.append(cul)
+    if remaining:
+        cul += 1
+        if random.randint(0, 7) == 0:
+            stood[i // 60] += 1
+        remaining -= 1
 
+    else:
+        prob = random.randint(0, 200 - activityMultipliers[i//60]*20//1)
+        if prob == 0:
+            remaining = random.randint(0, 1+7*activityMultipliers[i//60]//1)
+            stood[i//60]+=1
 
-# stood = [0] * 24
+# convert data
 
-# for i in range(8,22):
-#     stood[i] = random.randint(1, 8)
+print(minutely[-1])
+
+minutely = [60*i-x*60 for i, x in enumerate(minutely)]
+
+print(minutely[:100])
+
 
 tupScore = []
 for i in range(7):
     tupScore.append(random.randint(15,30))
-logs_ref.document('h2vVRIIuNyr65vgZCe2Y').update({
-    # u'minutely': minutely,
-    u'tupGo': tupScore,
-    # u'standFreq': stood,
+logs_ref.document('xOz6Cjsm6Zom8njn2MsX').update({
+    u'minutely': minutely,
+    # u'tupGo': tupScore,
+    u'standFreq': stood,
 
 })
